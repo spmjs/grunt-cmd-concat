fda28feefefed3132275bb0b25ddec943876a1fb
Hsiaoming Yang <lepture@me.com>
2013-03-12 20:04:22 +0800

    prepare for css concat

---
+++ tasks/lib/style.js
@@ -0,0 +1,11 @@
+
+exports.init = function(grunt) {
+  var exports = {};
+
+  exports.cssConcat = function(fileObj, options) {
+    var data = grunt.file.read(fileObj.src);
+
+  };
+
+  return exports;
+};
